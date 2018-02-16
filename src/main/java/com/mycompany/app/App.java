629062e2f8f583ea45114d5f0563d019c05fe448
BurakDmb <burakhan.demirbilek@gmail.com>
2018-02-16 21:01:44 +0300

    Typo fix for input parameter name

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -58,10 +58,10 @@ public class App
                 inputList1.add(value);
             }
         }
-        System.out.println(inputList1);
+        //System.out.println(inputList1);
 
 
-        String input2 = req.queryParams("input1");
+        String input2 = req.queryParams("input2");
         java.util.Scanner sc2 = new java.util.Scanner(input2);
         sc1.useDelimiter("[;\r\n]+");
         java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
@@ -74,7 +74,7 @@ public class App
             }
 
         }
-        System.out.println(inputList2);
+        //System.out.println(inputList2);
 
 
         String input3 = req.queryParams("input3").replaceAll("\\s","");
